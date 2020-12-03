ac6da2aa3cd3720af8924415d37917e0fcb4062e
0x28 <12886563+0x28@users.noreply.github.com>
2020-12-03 18:38:24 +0100

    Only iterate the field once for day 3

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,30 +1,22 @@
 use aoc_2020::input_file;
 use std::fs;
 
-type Field = Vec<Vec<char>>;
-
-fn collisions<F>(field: &Field, predicate: F) -> usize
-where
-    F: Fn(&(usize, &Vec<char>)) -> bool,
-{
-    field.iter().enumerate().filter(predicate).count()
-}
-
 fn main() {
-    let field = fs::read_to_string(input_file("day3.txt"))
+    let (s1, s2, s3, s4, s5) = fs::read_to_string(input_file("day3.txt"))
         .expect("file not found!")
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
-        .collect();
+        .enumerate()
+        .fold((0, 0, 0, 0, 0), |(s1, s2, s3, s4, s5), (y, line)| {
+            (
+                (line[y * 3 % line.len()] == '#') as u32 + s1,
+                (line[y % line.len()] == '#') as u32 + s2,
+                (line[y * 5 % line.len()] == '#') as u32 + s3,
+                (line[y * 7 % line.len()] == '#') as u32 + s4,
+                (y % 2 == 0 && line[y / 2 % line.len()] == '#') as u32 + s5,
+            )
+        });
 
-    let s1 = collisions(&field, |(y, line)| line[y * 3 % line.len()] == '#');
-    let s2 = collisions(&field, |(y, line)| line[y % line.len()] == '#');
-    let s3 = collisions(&field, |(y, line)| line[y * 5 % line.len()] == '#');
-    let s4 = collisions(&field, |(y, line)| line[y * 7 % line.len()] == '#');
-    let s5 = collisions(&field, |(y, line)| {
-        y % 2 == 0 && line[y / 2 % line.len()] == '#'
-    });
-
-    println!("part1 = {}", s1);
-    println!("part2 = {}", s1 * s2 * s3 * s4 * s5);
+    println!("part 1 = {}", s1);
+    println!("part 2 = {}", s1 * s2 * s3 * s4 * s5);
 }
