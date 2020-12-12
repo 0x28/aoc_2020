74be7d35b79576aeebacf8729a4ec5466200ca53
0x28 <12886563+0x28@users.noreply.github.com>
2020-12-12 12:40:53 +0100

    Refactor day 12

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -16,15 +16,15 @@ fn parse(input: &str) -> Vec<Action> {
     input
         .lines()
         .map(|line| {
-            let begin = line.chars().next()?;
-            match begin {
-                'N' => Some(Action::North(line[1..].parse().ok()?)),
-                'S' => Some(Action::South(line[1..].parse().ok()?)),
-                'E' => Some(Action::East(line[1..].parse().ok()?)),
-                'W' => Some(Action::West(line[1..].parse().ok()?)),
-                'L' => Some(Action::Left(line[1..].parse().ok()?)),
-                'R' => Some(Action::Right(line[1..].parse().ok()?)),
-                'F' => Some(Action::Forward(line[1..].parse().ok()?)),
+            let (first, rest) = line.split_at(1);
+            match first {
+                "N" => Some(Action::North(rest.parse().ok()?)),
+                "S" => Some(Action::South(rest.parse().ok()?)),
+                "E" => Some(Action::East(rest.parse().ok()?)),
+                "W" => Some(Action::West(rest.parse().ok()?)),
+                "L" => Some(Action::Left(rest.parse().ok()?)),
+                "R" => Some(Action::Right(rest.parse().ok()?)),
+                "F" => Some(Action::Forward(rest.parse().ok()?)),
                 _ => None,
             }
         })
@@ -42,8 +42,8 @@ fn part1(actions: &[Action]) -> i32 {
             Action::South(v) => pos.1 -= v,
             Action::East(v) => pos.0 += v,
             Action::West(v) => pos.0 -= v,
-            Action::Left(d) => degree = (degree - *d + 360) % 360,
-            Action::Right(d) => degree = (degree + *d) % 360,
+            Action::Left(d) => degree = (degree - *d).rem_euclid(360),
+            Action::Right(d) => degree = (degree + *d).rem_euclid(360),
             Action::Forward(v) if degree == 0 => pos.1 += v,
             Action::Forward(v) if degree == 90 => pos.0 += v,
             Action::Forward(v) if degree == 180 => pos.1 -= v,
@@ -55,14 +55,8 @@ fn part1(actions: &[Action]) -> i32 {
     i32::abs(pos.0) + i32::abs(pos.1)
 }
 
-fn rotate(point: (i32, i32), mut degree: i32) -> (i32, i32) {
-    let mut res = point;
-    while degree > 0 {
-        res = (res.1, -res.0);
-        degree -= 90;
-    }
-
-    res
+fn rotate(point: (i32, i32), degree: i32) -> (i32, i32) {
+    (0..degree.rem_euclid(360) / 90).fold(point, |(x, y), _| (y, -x))
 }
 
 fn part2(actions: &[Action]) -> i32 {
