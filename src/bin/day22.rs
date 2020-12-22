31c8e3b02d92792e14c660fe77e2052044b21ce5
0x28 <12886563+0x28@users.noreply.github.com>
2020-12-22 17:10:56 +0100

    Add day 22 part 2

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,5 +1,5 @@
 use aoc_2020::input_file;
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 use std::fs;
 
 fn parse_player(player: &str) -> Vec<u32> {
@@ -16,9 +16,17 @@ fn parse(input: &str) -> (Vec<u32>, Vec<u32>) {
     (parse_player(players[0]), parse_player(players[1]))
 }
 
+fn score_deck(deck: &[u32]) -> u32 {
+    deck.iter()
+        .rev()
+        .enumerate()
+        .map(|(n, &card)| (n as u32 + 1) * card)
+        .sum()
+}
+
 fn part1(player1: &[u32], player2: &[u32]) -> u32 {
-    let mut deck1 = player1.iter().collect::<VecDeque<_>>();
-    let mut deck2 = player2.iter().collect::<VecDeque<_>>();
+    let mut deck1 = player1.iter().copied().collect::<VecDeque<_>>();
+    let mut deck2 = player2.iter().copied().collect::<VecDeque<_>>();
 
     while !deck1.is_empty() && !deck2.is_empty() {
         let card1 = deck1.pop_front().unwrap();
@@ -34,26 +42,79 @@ fn part1(player1: &[u32], player2: &[u32]) -> u32 {
     }
 
     if deck1.is_empty() {
-        deck2
-            .into_iter()
-            .rev()
-            .enumerate()
-            .map(|(n, &card)| (n as u32 + 1) * card)
-            .sum()
+        score_deck(deck2.make_contiguous())
     } else {
-        deck1
-            .into_iter()
-            .rev()
-            .enumerate()
-            .map(|(n, &card)| (n as u32 + 1) * card)
-            .sum()
+        score_deck(deck1.make_contiguous())
     }
+}
+
+fn recursive_combat(player1: &[u32], player2: &[u32]) -> (u8, VecDeque<u32>) {
+    let mut deck1 = player1.iter().copied().collect::<VecDeque<_>>();
+    let mut deck2 = player2.iter().copied().collect::<VecDeque<_>>();
+    let mut dejavu1 = HashSet::<VecDeque<u32>>::new();
+    let mut dejavu2 = HashSet::<VecDeque<u32>>::new();
+
+    while !deck1.is_empty() && !deck2.is_empty() {
+        if dejavu1.contains(&deck1) || dejavu2.contains(&deck2) {
+            return (1, deck1);
+        } else {
+            dejavu1.insert(deck1.clone());
+            dejavu2.insert(deck2.clone());
+        }
+
+        let card1 = deck1.pop_front().unwrap();
+        let card2 = deck2.pop_front().unwrap();
+
+        if card1 as usize <= deck1.len() && card2 as usize <= deck2.len() {
+            let rdeck1 = deck1
+                .iter()
+                .take(card1 as usize)
+                .cloned()
+                .collect::<Vec<u32>>();
+            let rdeck2 = deck2
+                .iter()
+                .take(card2 as usize)
+                .cloned()
+                .collect::<Vec<u32>>();
+
+            match recursive_combat(&rdeck1, &rdeck2) {
+                (1, _) => {
+                    deck1.push_back(card1);
+                    deck1.push_back(card2);
+                }
+                (2, _) => {
+                    deck2.push_back(card2);
+                    deck2.push_back(card1);
+                }
+                _ => unreachable!(),
+            }
+        } else if card1 > card2 {
+            deck1.push_back(card1);
+            deck1.push_back(card2);
+        } else {
+            deck2.push_back(card2);
+            deck2.push_back(card1);
+        }
+    }
+
+    if deck2.is_empty() {
+        (1, deck1)
+    } else {
+        (2, deck2)
+    }
+}
+
+fn part2(player1: &[u32], player2: &[u32]) -> u32 {
+    let (_, mut deck) = recursive_combat(player1, player2);
+
+    score_deck(deck.make_contiguous())
 }
 
 fn main() {
     let input = fs::read_to_string(input_file("day22.txt")).unwrap();
     let player = parse(&input);
     println!("part1 = {}", part1(&player.0, &player.1));
+    println!("part2 = {}", part2(&player.0, &player.1));
 }
 
 #[test]
@@ -75,4 +136,5 @@ Player 2:
     let example1 = parse(example1);
 
     assert_eq!(part1(&example1.0, &example1.1), 306);
+    assert_eq!(part2(&example1.0, &example1.1), 291);
 }
