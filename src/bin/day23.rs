54354385841e2bcb680389d34d3441bbc72d7934
0x28 <12886563+0x28@users.noreply.github.com>
2020-12-23 22:38:08 +0100

    Rewrite day 23 part 1 using a list instead of vec

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,57 +1,70 @@
-fn parse(input: &str) -> Vec<u32> {
-    input.chars().flat_map(|c| c.to_digit(10)).collect()
+#[derive(Debug, Clone, PartialEq)]
+struct Cup {
+    value: i64,
+    next: usize,
 }
 
-fn part1(numbers: &[u32]) -> String {
-    let mut cups = numbers.iter().copied().collect::<Vec<_>>();
-    let mut current_cup = *cups.first().unwrap();
+fn parse(input: &str) -> Vec<Cup> {
+    let mut cups = vec![];
 
-    for _ in 0..100 {
-        let mut crab_cups = vec![];
+    for (idx, c) in input.chars().enumerate() {
+        let value = c.to_digit(10).unwrap() as i64;
+        let next = (idx + 1) % input.len();
 
-        for _ in 0..3 {
-            let next_index =
-                (cups.iter().position(|&c| c == current_cup).unwrap() + 1)
-                    % cups.len();
-            crab_cups.push(cups.remove(next_index));
-        }
-
-        let mut dest = if current_cup == 1 {
-            numbers.len() as u32
-        } else {
-            current_cup - 1
-        };
-        let dest_idx;
-        loop {
-            if let Some(idx) = cups.iter().position(|&c| c == dest) {
-                dest_idx = idx;
-                break;
-            } else {
-                dest = if dest == 1 {
-                    numbers.len() as u32
-                } else {
-                    dest - 1
-                };
-            }
-        }
-
-        for _ in 0..3 {
-            cups.insert(dest_idx + 1, crab_cups.pop().unwrap());
-        }
-
-        let next_index = (cups.iter().position(|&c| c == current_cup).unwrap()
-            + 1)
-            % cups.len();
-        current_cup = cups[next_index];
+        cups.push(Cup { value, next })
     }
 
-    let one_idx = cups.iter().position(|&c| c == 1).unwrap();
-    let mut result = String::new();
-    let mut idx = one_idx + 1;
+    cups
+}
 
-    while cups[idx % cups.len()] != 1 {
-        result.push(std::char::from_digit(cups[idx % cups.len()], 10).unwrap());
-        idx += 1;
+fn part1(numbers: &[Cup]) -> String {
+    let mut cups = numbers.to_vec();
+    let mut current_cup_idx = 0;
+
+    for _ in 0..100 {
+        let crab1_idx = cups[current_cup_idx].next;
+        let crab2_idx = cups[crab1_idx].next;
+        let crab3_idx = cups[crab2_idx].next;
+
+        let mut dest_idx = cups[crab3_idx].next;
+        let mut search_value = cups[current_cup_idx].value - 1;
+
+        loop {
+            let search_cup = &cups[dest_idx];
+
+            if search_cup == &cups[current_cup_idx] {
+                // wrap
+                search_value = if search_value <= 1 {
+                    numbers.len() as i64
+                } else {
+                    search_value - 1
+                };
+            } else if search_cup.value == search_value
+                && search_cup != &cups[crab1_idx]
+                && search_cup != &cups[crab2_idx]
+                && search_cup != &cups[crab3_idx]
+            {
+                break;
+            }
+
+            dest_idx = search_cup.next;
+        }
+
+        let after_crab3 = cups[crab3_idx].next;
+        cups[crab3_idx].next = cups[dest_idx].next;
+        cups[dest_idx].next = cups[current_cup_idx].next; // index of crab3
+        cups[current_cup_idx].next = after_crab3;
+
+        current_cup_idx = cups[current_cup_idx].next;
+    }
+
+    let one_idx = cups.iter().position(|c| c.value == 1).unwrap();
+    let mut result = String::new();
+    let mut idx = cups[one_idx].next;
+
+    while cups[idx].value != 1 {
+        result.push(std::char::from_digit(cups[idx].value as u32, 10).unwrap());
+        idx = cups[idx].next;
     }
 
     result
