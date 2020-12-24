5f321b975a39f1c1ed16c1a15c591ef0c211111f
0x28 <12886563+0x28@users.noreply.github.com>
2020-12-24 12:52:54 +0100

    Add day24 part 2

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -1,8 +1,5 @@
 use aoc_2020::input_file;
-use std::{
-    collections::{HashMap, HashSet},
-    fs,
-};
+use std::{collections::{HashMap, HashSet}, fs, hash::Hash};
 
 #[derive(Debug, PartialEq)]
 enum Dir {
@@ -65,7 +62,7 @@ fn parse(input: &str) -> Vec<Vec<Dir>> {
     input.lines().map(parse_line).collect()
 }
 
-fn find_black_tiles(dirs: Vec<Vec<Dir>>) -> HashSet<(i64, i64)> {
+fn find_black_tiles(dirs: &Vec<Vec<Dir>>) -> HashSet<(i64, i64)> {
     let mut flipped = HashMap::<(i64, i64), bool>::new();
     let ref_tile = (0, 0);
 
@@ -92,13 +89,70 @@ fn find_black_tiles(dirs: Vec<Vec<Dir>>) -> HashSet<(i64, i64)> {
         .collect()
 }
 
-fn part1(dirs: Vec<Vec<Dir>>) -> usize {
-    find_black_tiles(dirs).len()
+fn part1(dirs: &Vec<Vec<Dir>>) -> usize {
+    find_black_tiles(&dirs).len()
+}
+
+fn conway<P, N>(field: &HashSet<P>, neighbors_fun: N) -> usize
+where
+    N: Fn(&P) -> HashSet<P> + Copy,
+    P: Clone + Copy + Eq + Hash,
+{
+    let mut current = field.clone();
+
+    for _ in 0..100 {
+        let mut next = HashSet::new();
+        for black_tile in current.iter() {
+            let neighbors = neighbors_fun(&black_tile);
+            if let 1 | 2 = neighbors.intersection(&current).count() {
+                next.insert(black_tile);
+            }
+        }
+        let extended_cubes = current
+            .iter()
+            .flat_map(neighbors_fun)
+            .collect::<HashSet<_>>();
+        for inactive_cube in extended_cubes.difference(&current) {
+            let neighbors = neighbors_fun(inactive_cube);
+            if let 2 = neighbors.intersection(&current).count() {
+                next.insert(inactive_cube);
+            }
+        }
+
+        current = next.into_iter().copied().collect();
+    }
+
+    current.len()
+}
+
+fn neighbors((pos_x, pos_y): &(i64, i64)) -> HashSet<(i64, i64)> {
+    let dirs = vec![
+        Dir::East,
+        Dir::SouthEast,
+        Dir::SouthWest,
+        Dir::West,
+        Dir::NorthWest,
+        Dir::NorthEast,
+    ];
+
+    dirs.iter()
+        .map(|d| {
+            let (rx, ry) = dir_to_pos(d);
+            (pos_x + rx, pos_y + ry)
+        })
+        .collect()
+}
+
+fn part2(dirs: &Vec<Vec<Dir>>) -> usize {
+    let black_tiles = find_black_tiles(&dirs);
+
+    conway(&black_tiles, neighbors)
 }
 
 fn main() {
     let dirs = parse(&fs::read_to_string(input_file("day24.txt")).unwrap());
-    println!("part1 = {}", part1(dirs));
+    println!("part1 = {}", part1(&dirs));
+    println!("part2 = {}", part2(&dirs));
 }
 
 #[test]
